fd3b013ccb5708bf6c6e64f586717dc8796e1617
sashkaochigava <sashkaochi@gmail.com>
2024-04-30 18:39:38 +0400

    feat: created ShopView and imported ProductCard component

--- src/views/ShopView.vue
+++ src/views/ShopView.vue
@@ -1,7 +1,20 @@
-<script lang="ts" setup></script>
+<script lang="ts" setup>
+import { useProductStore } from "../pinia/products.pinia.ts";
+import ProductCard from "../components/shop/ProductCard.vue";
+
+const productStore = useProductStore();
+
+const products = productStore.products;
+</script>
 
 <template>
-  <main>
-    <h1>xui</h1>
+  <main class="grid grid-cols-3 gap-y-3 gap-x-4 px-16 py-5">
+    <ProductCard
+      v-for="product in products"
+      :key="product.id"
+      :name="product.name"
+      :photo="product.photo"
+      :price="product.price"
+    />
   </main>
 </template>
