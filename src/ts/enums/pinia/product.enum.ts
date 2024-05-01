6f742241a2c0c6b4830e47dac1e626539d72e1dc
sashkaochigava <sashkaochi@gmail.com>
2024-05-02 01:38:07 +0400

    feat: created interfaces for product store

---
+++ src/ts/enums/pinia/product.enum.ts
@@ -0,0 +1,9 @@
+export enum EProductActions {
+  SetProducts = "setProducts",
+  SetSelectedProduct = "setSelectedProduct",
+}
+
+export enum EProductGetters {
+  GetProducts = "getProducts",
+  GetSelectedProduct = "getSelectedProduct",
+}
