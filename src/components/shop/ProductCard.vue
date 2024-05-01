6e6e47b2a76fe6e7600a69228d7d1db40bb06fc2
sashkaochigava <sashkaochi@gmail.com>
2024-05-01 16:20:56 +0400

    feat: added function to view single product page

--- src/components/shop/ProductCard.vue
+++ src/components/shop/ProductCard.vue
@@ -1,13 +1,29 @@
 <script lang="ts" setup>
+import { useRouter } from "vue-router";
+import { useProductStore } from "../../pinia/products.pinia.ts";
+
 const props = defineProps<{
   name: string;
   price: number;
   photo?: string;
+  id: number;
 }>();
+
+const router = useRouter();
+
+const productStore = useProductStore();
+
+const goToProductPage = () => {
+  router.push({ name: "product", params: { id: props.id } });
+  productStore.setSelectedProduct(props.id);
+};
 </script>
 
 <template>
-  <article class="h-96 rounded-lg cursor-pointer box-shadow-all-sides">
+  <article
+    class="h-96 rounded-lg cursor-pointer box-shadow-all-sides"
+    @click="goToProductPage"
+  >
     <div v-if="!props.photo" class="w-full h-72 bg-blue-500 rounded-t-lg"></div>
     <img v-else :src="photo" alt="not found" class="w-full h-72 rounded-t-lg" />
     <section class="flex flex-col p-3 gap-2">
