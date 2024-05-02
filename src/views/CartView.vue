b99f6666764711e53f6b6e6b2eb3783da1281dff
sashkaochigava <sashkaochi@gmail.com>
2024-05-02 13:40:17 +0400

    refactor: refactored CartView

--- src/views/CartView.vue
+++ src/views/CartView.vue
@@ -1,11 +1,16 @@
 <script lang="ts" setup>
-import { computed, onMounted } from "vue";
+import { computed, onMounted, ref } from "vue";
 import axios from "axios";
 import { useCartStore } from "../pinia/cart.pinia.ts";
 import { ECartActions, ECartGetters } from "../ts/enums/pinia/cart.enum.ts";
 import { IProduct } from "../ts/interfaces/product.interface.ts";
+import { useRouter } from "vue-router";
+
+const router = useRouter();
 
 const cartStore = useCartStore();
+
+const cartIsEmpty = ref<boolean>(false);
 
 const cart = computed<IProduct[]>(() => cartStore[ECartGetters.GetCart]);
 
@@ -56,29 +61,63 @@ const removeProductFromCart = (id: string | null) => {
     console.log(err);
   }
 };
+
+const goToCheckout = (): void => {
+  if (cart.value.length < 1) {
+    cartIsEmpty.value = true;
+    return;
+  }
+  router.push({ name: "checkout" });
+};
 </script>
 
 <template>
-  <div v-for="cartItem in cart" :key="cartItem._id">
-    <h1>{{ cartItem?.name }}</h1>
-    <div
-      class="w-28 bg-gray-200 rounded-md flex items-center justify-center gap-5 py-2"
+  <main class="flex flex-col gap-10">
+    <h1>Cart</h1>
+    <section
+      v-for="cartItem in cart"
+      :key="cartItem._id"
+      class="w-fit flex items-center gap-10"
     >
-      <i
-        class="fa-solid fa-minus text-sm cursor-pointer"
-        @click.left="decreaseQuantity(cartItem.cart_id ?? null)"
-      ></i>
-      <p>{{ cartItem?.quantity }}</p>
-      <i
-        class="fa-solid fa-plus text-sm cursor-pointer"
-        @click.left="increaseQuantity(cartItem.cart_id ?? null)"
-      ></i>
-    </div>
-    <h1
-      class="cursor-pointer"
-      @click.left="removeProductFromCart(cartItem.cart_id ?? null)"
+      <img
+        :src="cartItem?.gallery[0]"
+        alt="not found"
+        class="w-[150px] h-[150px] rounded-md box-shadow-all-sides"
+      />
+      <div class="flex flex-col gap-6">
+        <h2 class="text-lg">{{ cartItem?.name }}</h2>
+
+        <div class="flex items-center gap-12">
+          <div
+            class="w-20 bg-gray-200 rounded-md flex items-center justify-center gap-3 py-1"
+          >
+            <i
+              class="fa-solid fa-minus text-xs cursor-pointer"
+              @click.left="decreaseQuantity(cartItem.cart_id ?? null)"
+            ></i>
+            <p class="text-sm">{{ cartItem?.quantity }}</p>
+            <i
+              class="fa-solid fa-plus text-xs cursor-pointer"
+              @click.left="increaseQuantity(cartItem.cart_id ?? null)"
+            ></i>
+          </div>
+          <h2>{{ cartItem.price * cartItem.quantity }}$</h2>
+        </div>
+
+        <h2
+          class="text-light-blue cursor-pointer transition-all hover:text-black"
+          @click.left="removeProductFromCart(cartItem.cart_id ?? null)"
+        >
+          Remove
+        </h2>
+      </div>
+    </section>
+    <button
+      class="w-40 text-white rounded-xl bg-light-blue px-5 py-2"
+      @click="goToCheckout"
     >
-      remove
-    </h1>
-  </div>
+      Checkout
+    </button>
+    <p v-if="cartIsEmpty" class="text-red-700">add products to cart</p>
+  </main>
 </template>
